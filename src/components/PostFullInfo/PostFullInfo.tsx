3f99f3444225b7bd8f8294a7498a1f94ee892517
kate0321vs <ekaterina.chechulina2103@gmail.com>
2025-02-07 23:27:27 +0600

    Реализовала удаление поста с API по клику на кнопку

--- src/components/PostFullInfo/PostFullInfo.tsx
+++ src/components/PostFullInfo/PostFullInfo.tsx
@@ -1,31 +1,50 @@
 import { Button, Card, CardActions, CardContent, Typography } from '@mui/material';
 import dayjs from 'dayjs';
-import { NavLink, useParams } from 'react-router-dom';
-import { IPost } from '../../types';
+import { NavLink, useNavigate, useParams } from 'react-router-dom';
+import { IPost, IPostsApi } from '../../types';
 import { useCallback, useEffect, useState } from 'react';
 import axiosApi from '../../axiosApi.ts';
 import Loader from '../UI/Loader/Loader.tsx';
+import { toast } from 'react-toastify';
 
 const PostFullInfo = () => {
   const [loading, setLoading] = useState(false);
   const [post, setPost] = useState<IPost | null>(null);
-  const { id } = useParams();
+  const navigate = useNavigate();
+  const {id} = useParams();
 
   const fetchPostInfo = useCallback(async () => {
-      try {
-        setLoading(true);
-        const response = await axiosApi.get(`posts/${id}.json`);
-        setPost(response.data);
-      } catch (e) {
-        alert(e);
-      } finally {
-        setLoading(false);
-      }
-    }, [id])
+    if (!id) {
+      return
+    }
+    try {
+      setLoading(true);
+      const response = await axiosApi.get(`posts/${id}.json`);
+      setPost(response.data);
+    } catch (e) {
+      alert(e);
+    } finally {
+      setLoading(false);
+    }
+
+  }, [id])
 
   useEffect(() => {
     void fetchPostInfo()
   }, [fetchPostInfo]);
+
+  const DeleteGame = async () => {
+    try {
+      setLoading(true);
+      await axiosApi.delete<IPostsApi>(`posts/${id}.json`);
+      toast.info('Post was deleted Successfully!');
+      navigate('/')
+    } catch (e) {
+      alert(e);
+    } finally {
+      setLoading(false);
+    }
+  }
 
   let postInfo = null;
 
@@ -49,12 +68,12 @@ const PostFullInfo = () => {
           </CardContent>
           <CardActions>
             <Button component={NavLink} to={`/posts/${id}/edit`} size="small">Edit</Button>
-            <Button size="small">Delete</Button>
+            <Button color="error" size="small" onClick={DeleteGame}>Delete</Button>
           </CardActions>
         </Card>
       );
     } else {
-     postInfo = (<p>Post not found</p>)
+      postInfo = (<p>Post not found</p>)
     }
   }
 
